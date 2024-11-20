4d3c687e831fc4d2266cf3a9821206fd24c6b7b6
Uchenna99 <uchennaagbu@gmail.com>
2024-11-20 17:35:57 +0100

    code

--- coding.js
+++ coding.js
@@ -6,3 +6,18 @@ function fizzArray2(n){
     return result;
 }
 console.log(fizzArray2(10));
+
+
+
+function mirrorEnds(str){
+    let start = 0;
+    let end = str.length - 1;
+
+    while (start <= end && str[start] === str[end]) {
+        start++;
+        end--;
+    }
+    return str.slice(0, start);
+}
+
+
