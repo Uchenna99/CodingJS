b23de87208f6605a51218c45728ee8cc944ac99c
Uchenna99 <uchennaagbu@gmail.com>
2024-11-21 22:52:22 +0100

    ghappy

--- coding.js
+++ coding.js
@@ -23,6 +23,25 @@ function mirrorEnds(str) {
     
     return mirror;
 }
+console.log(mirrorEnds('aba'));
+
+
+
+
+function gHappy(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'g') {
+            if (str[i - 1] !== 'g' && str[i + 1] !== 'g') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+console.log(gHappy('xxggyygxx'));
+
+
+
 
 
 
