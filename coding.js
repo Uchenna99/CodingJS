a9c0f27eccd4f174ef1907b998cd76d4cfb10be4
Uchenna99 <uchennaagbu@gmail.com>
2024-11-22 11:08:38 +0100

    count evens

--- coding.js
+++ coding.js
@@ -56,4 +56,19 @@ console.log(fizzArray3(5, 10));
 
 
 
+// solve with javascript Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1. Examples countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+function countEvens(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countEvens([2, 1, 2, 3, 4]));
+
+
+
+
 
