0d21502ec8245286b57f1c7a5c4225559586f634
Uchenna99 <uchennaagbu@gmail.com>
2024-11-28 22:10:57 +0100

    no14

--- coding.js
+++ coding.js
@@ -85,3 +85,21 @@ function bigDiff(arr) {
     
     return maxVal - minVal;
 }
+
+
+
+
+// Given an array of ints, return true if it contains no 1's or it contains no 4's.
+
+// Examples
+
+// no14([1, 2, 3]) → true
+// no14([1, 2, 3, 4]) → false
+// no14([2, 3, 4]) → true
+
+function no14(nums) {
+    const containsOne = nums.includes(1);
+    const containsFour = nums.includes(4);
+    return !(containsOne && containsFour);
+}
+console.log(no14([1, 2, 3, 4]));
